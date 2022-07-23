dacc5962841e6f80aa8a7fa56c6018075a706773
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-22 18:31:17 -0700

    Create List & Detail views

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render  # noqa:F401
+from rest_framework import generics
 
-# Create your views here.
+from .models import Todo
+from .serializers import TodoSerializer
+
+
+class ListTodo(generics.ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class DetailTodo(generics.RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
