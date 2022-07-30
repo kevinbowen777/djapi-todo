a044b91eb1145bec6e824cc707c48672caeb24fb
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-30 08:25:06 -0700

    Wire HTML view to root URL

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from todos.views import TodoListView
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("todos.urls")),
+    path("", TodoListView.as_view()),
 ]
