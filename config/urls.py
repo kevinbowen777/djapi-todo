3def4ba58d159dbd169b8e4425695095a048e2b3
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-30 09:35:21 -0700

    Add api-auth path. Fixes #7

--- config/urls.py
+++ config/urls.py
@@ -7,5 +7,6 @@ from todos.views import TodoListView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("todos.urls")),
+    path("api-auth/", include("rest_framework.urls")),
     path("", TodoListView.as_view()),
 ]
