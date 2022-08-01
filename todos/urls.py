f7a6af8ef1dce10a084b6815c08c7f00c413db09
Kevin Bowen <kevin.bowen@gmail.com>
2022-08-01 16:07:43 -0700

    Add URL routes for User list & detail

--- todos/urls.py
+++ todos/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import ListTodo, DetailTodo
+from .views import ListTodo, DetailTodo, UserList, UserDetail
 
 
 urlpatterns = [
+    path("users/", UserList.as_view()),
+    path("users/<int:pk>/", UserDetail.as_view()),
     path("<int:pk>/", DetailTodo.as_view(), name="todo_detail"),
     path("", ListTodo.as_view(), name="todo_list"),
 ]
