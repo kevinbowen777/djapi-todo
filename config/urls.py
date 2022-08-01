bdd542e06659a64df0bf5596d988c3dbe079fad2
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-31 18:50:16 -0700

    Implement token authentication. Fixes #14

--- config/urls.py
+++ config/urls.py
@@ -8,5 +8,10 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("todos.urls")),
     path("api-auth/", include("rest_framework.urls")),
+    path("api/v1/dj-rest-auth/", include("dj_rest_auth.urls")),
+    path(
+        "api/v1/dj-rest-auth/registration/",
+        include("dj_rest_auth.registration.urls"),
+    ),
     path("", TodoListView.as_view()),
 ]
