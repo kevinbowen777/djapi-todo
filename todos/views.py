d8a6a6a5eefbe75e97359e4b8a3f9cfdd2f4b609
Kevin Bowen <kevin.bowen@gmail.com>
2022-08-01 16:05:27 -0700

    Create UserList & UserDetail views

--- todos/views.py
+++ todos/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.views.generic import ListView
 from rest_framework import generics
 
 from .models import Todo
-from .serializers import TodoSerializer
+from .serializers import TodoSerializer, UserSerializer
 
 
 class ListTodo(generics.ListCreateAPIView):
@@ -13,6 +14,16 @@ class ListTodo(generics.ListCreateAPIView):
 class DetailTodo(generics.RetrieveUpdateDestroyAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+
+
+class UserList(generics.ListCreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
 
 
 class TodoListView(ListView):
