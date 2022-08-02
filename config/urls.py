df68bd111b9d3826166066e5c0f0330bebdec5fe
Kevin Bowen <kevin.bowen@gmail.com>
2022-08-01 19:11:13 -0700

    Create manual schema & dynamic schema URL

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView
 
 from todos.views import TodoListView
 
@@ -12,6 +13,11 @@ urlpatterns = [
     path(
         "api/v1/dj-rest-auth/registration/",
         include("dj_rest_auth.registration.urls"),
+    ),
+    path(
+        "api/schema/",
+        SpectacularAPIView.as_view(),
+        name="schema",
     ),
     path("", TodoListView.as_view()),
 ]
